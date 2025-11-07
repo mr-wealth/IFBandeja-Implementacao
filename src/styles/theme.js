9d8985d81aaaef90be72d0697dee7dc25c22e60f
ThiagoMaschietto <thiago000maschietto@gmail.com>
2025-11-07 11:53:32 -0300

    Pagina Login sem Conexão com Backend

---
+++ src/styles/theme.js
@@ -0,0 +1,17 @@
+
+export const lightTheme = {
+  background: '#FFFFFF', 
+  text: '#333333',       
+  inputBackground: 'transparent',
+  inputBorder: '#CCCCCC', 
+  logoFilter: 'none', 
+};
+
+export const darkTheme = {
+  background: '#121212', 
+  text: '#FFFFFF',     
+  inputBackground: 'transparent', 
+  inputBorder: '#555555',    
+  
+  logoFilter: 'invert(1)', 
+};
